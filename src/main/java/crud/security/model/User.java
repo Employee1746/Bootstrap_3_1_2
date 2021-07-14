fac4866710928d16fae742353a8765ec22510fb1
Malh1n <58389533+Malh1n@users.noreply.github.com>
2021-07-14 15:48:02 +0300

    done

--- src/main/java/crud/security/model/User.java
+++ src/main/java/crud/security/model/User.java
@@ -30,7 +30,7 @@ public class User implements UserDetails {
 
     @JsonIgnore
     @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
